c8b61faa093f7a88cc9830f1257f380d853805b9
Hassan Alawie <hassan.alawie10@gmail.com>
2019-07-23 19:07:56 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -28,7 +28,7 @@ function doWhileLoop(num){
         }
       do {
     console.log("I run once regardless.")}
-    while (incrementVariable() <= num)
+    while (incrementVariable() <= num-1)
     
     
 }
